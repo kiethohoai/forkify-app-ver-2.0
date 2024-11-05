dd2fca7290fc9faa1327bacb3228155ad9af8191
kiethohoai <kietho.060193@gmail.com>
2024-11-05 10:09:02 +0700

    005 Loading a Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import { API_URL } from './config.js';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +13,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+/* ===== showRecipe =====  */
+const showRecipe = async function () {
+  try {
+    // const res = await fetch(`${API_URL}/664c8f193e7aa067e94e8706`);
+    const res = await fetch(`${API_URL}/664c8f193e7aa067e94e8438`);
+    const data = await res.json();
+
+    if (!res.ok)
+      throw Error(`${data.message} ${res.statusText}  ${res.status} `);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(`🚀  recipe =>`, recipe);
+  } catch (error) {
+    console.error(`🚀Error at showRecipe (controll.js) =>`, error);
+  }
+};
+showRecipe();
